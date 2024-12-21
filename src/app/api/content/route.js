b965992f458cc186fda62d1109d54369be93f92b
Spider Man <peter@gmail.com>
2024-12-21 23:56:16 +0530

    Complete channel route operations

--- src/app/api/content/route.js
+++ src/app/api/content/route.js
@@ -1,8 +1,32 @@
+import { dbConnect } from "@/lib/db"
+import Content from "@/lib/models/Content.model";
 import { NextResponse } from "next/server"
 
 export const GET = async () => {
+
+    await dbConnect();
+
+    const contents = await Content.find({});
+    
+    return NextResponse.json(contents, {
+        status : 200
+    })
+}
+
+
+export const POST = async (req) => {
+
+    const postBody = await req.json();
+    console.log("post body is : " + postBody)
+    
+    await dbConnect();
+
+    const newPost = await Content.create(postBody)
+
+    console.log(newPost)
     
     return NextResponse.json({
-        message : "youtube content api...."
+        message : "This is content post request....",
     })
+    
 }
--- src/lib/db.js
+++ src/lib/db.js
@@ -2,8 +2,8 @@ const { default: mongoose } = require("mongoose");
 
 export const dbConnect = async () => {
 
-    const MONGODB_URI = 'mongodb+srv://buddhikamadusanka2000:2001219_Buddhika@knowledgemanager.vfpdl.mongodb.net/?retryWrites=true&w=majority&appName=knowledgeManager';
-
+    const MONGODB_URI = 'mongodb+srv://buddhikamadusanka2000:2001219_Buddhika@knowledgemanager.vfpdl.mongodb.net/youtube_knowledge?retryWrites=true&w=majority&appName=knowledgeManager';
+    
     try{
         await mongoose.connect(MONGODB_URI);
         console.log("Database successfully connected.....");
