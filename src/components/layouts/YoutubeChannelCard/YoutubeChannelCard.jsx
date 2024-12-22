820195220b4bc5d44e47398942945dd4afabd125
Spider Man <peter@gmail.com>
2024-12-22 17:34:43 +0530

    Fix error occour from the Link component

--- src/components/layouts/YoutubeChannelCard/YoutubeChannelCard.jsx
+++ src/components/layouts/YoutubeChannelCard/YoutubeChannelCard.jsx
@@ -7,7 +7,7 @@ const YoutubeChannelCard = ({image, name, link, description}) => {
 
     // show youtube channel card only when props are available gor link and name..
     name && link && (        
-        <Link href={'#'} className='bg-mute px-10 py-14 rounded-3xl w-full youtub-chanel-card'>
+        <a href={'#'} className='bg-mute px-10 py-14 rounded-3xl w-full youtub-chanel-card'>
             <div className='w-3/5  mx-auto flex flex-col items-center'>
                 <Image 
                     src={'/sample.jpg'}
@@ -17,7 +17,7 @@ const YoutubeChannelCard = ({image, name, link, description}) => {
                     className='w-15 h-15 rounded-full object-cover'
                 />
 
-                <h2 className='font-semibold text-lg bg-mute text-center mt-5 text-lg line-clamp-2'>{name}</h2>
+                <h2 className='font-semibold text-lg bg-mute text-center mt-5 line-clamp-2'>{name}</h2>
 
                 {/* Show description when description is only available in the props */}
                 {
@@ -26,7 +26,7 @@ const YoutubeChannelCard = ({image, name, link, description}) => {
                     )
                 }
             </div>
-        </Link>
+        </a>
     )
     
   )
